0b6961b044b8848dcdb3f9a340f6ac7fcc1df65c
kimberly huynh and logan tanous <Guest@Epicodus-7E.local>
2018-02-28 15:47:38 -0800

    recipes buttons, add recipe form, and recipe list output

--- app.rb
+++ app.rb
@@ -1,14 +1,52 @@
 require('sinatra')
 require('sinatra/reloader')
-require('sinatra/activerecord')also_reload('lib/**.*.rb')
-require('lib/recipe_box')
+require('sinatra/activerecord')
+also_reload('lib/**.*.rb')
+require('./lib/recipes')
+require('./lib/instructions')
+require('./lib/ingredients')
+require('./lib/tags')
 require('pry')
 require('pg')
-
+# home page
 get('/') do
-  erb(:input)
+  erb(:home)
 end
 
-get('/output') do
-  erb(:output)
+# add new recipe button will go to /add and show add_recipe.erb page
+post('/add') do
+  erb(:add_recipe)
+end
+
+# subbmiting everything on /add and go to recipe_list
+# POST TO /add_recipe url from the recipe_list
+post('/add_recipe') do
+  new_instructions = params.fetch("instructions")
+  @instruction = Instruction.create({:description => new_instructions})
+
+  new_ingredients = params.fetch("ingredients")
+  @ingredient = Ingredient.create({:names => new_ingredients})
+
+  new_tags = params.fetch("tag")
+  @tag = Tag.create({:name => new_tags})
+
+  new_recipe = params.fetch("name")
+
+  @one_recipe = Recipe.create({:name => new_recipe, :instruction_id => @instruction.id, :ingredient_id => @ingredient.id, :tag_id => @tag.id})
+
+  # @ingredients = Ingredient.all
+  # @instructions = Instruction.all
+  # @recipes = Recipe.all
+  # @tags = Tag.all
+  erb(:recipe_list)
+end
+
+# will show stuff in recipe_list.erb
+get('/add_recipe') do
+  # @ingredient = Ingredient.all
+  # @instruction = Instruction.all
+  # @recipe = Recipe.all
+  # @tag = Tag.all
+  @one_recipe
+  erb(:recipe_list)
 end
