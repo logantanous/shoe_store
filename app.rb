3e6b4e04227657049fb0a2928b11dd651b2a5284
Logan Tanous <logantanous@gmail.com>
2018-03-02 12:40:03 -0800

    brands are assigning to stores

--- app.rb
+++ app.rb
@@ -13,14 +13,14 @@ end
 
 get '/stores' do
   @stores = Store.all
-  erb(:store)
+  erb(:stores)
 end
 
 post('/stores')do
   name = params[:title]
   @store = Store.create({:name => name})
   @stores = Store.all
-  erb(:store)
+  erb(:stores)
 end
 
 get '/brands' do
@@ -66,86 +66,32 @@ patch '/store/:id/edit' do
   erb(:home)
 end
 
-#
-# post('/') do
-#   new_recipe = params.fetch("recipe")
-#   @recipe = Recipe.create({:name => new_recipe})
-#   @recipes = Recipe.all
-#   erb(:home)
-# end
-#
-# delete('/:id') do
-#   @recipe = Recipe.find(params.fetch("id").to_i)
-#   @recipe.delete
-#   @recipes = Recipe.all
-#   erb(:home)
-# end
-#
-# get('/recipe/:id') do
-#   @recipe = Recipe.find(params.fetch("id").to_i)
-#   @recipes = Recipe.all
-#   @ingredients = Ingredient.all
-#   @instructions = Instruction.all
-#   # @tags = Tag.all
-#   erb(:add_recipe)
-# end
-#
-# post '/add_recipe/:id' do
-#   @recipe = Recipe.find(params.fetch("id").to_i)
-#   binding.pry
-#   new_ingredient = Ingredient.new({:names => params['new_ingredient']})
-#   @recipe.ingredient_id = new_ingredient.id
-#   # binding.pry
-#   # @ingredient_recipe = Ingredient_recipes.create({:recipe_id => @recipe.id, :ingredient_id => @ingredient.id})
-#   # @ingredients = Ingredient.all
-#   # @instructions = Instruction.all
-#   # @tags = Tag.all
-#   # @recipes = Recipe.all
-#   erb(:add_recipe)
-# end
-#
-# # add new recipe button will go to /add and show add_recipe.erb page
-# post('/add') do
-#   erb(:add_recipe)
-# end
-#
-# # # subbmiting everything on /add and go to recipe_list
-# # # POST TO /add_recipe url from the recipe_list
-# # post('/add_recipe/:id') do
-# #   @recipe = Recipe.find(params.fetch("id").to_i)
-# #
-# #   new_instructions = params.fetch("instructions")
-# #   @instruction = Instruction.create({:description => new_instructions})
-# #
-# #   new_ingredients = params.fetch("ingredients")
-# #   @ingredient = Ingredient.create({:names => new_ingredients})
-# #
-# #   new_tags = params.fetch("tag")
-# #   @tag = Tag.create({:name => new_tags})
-# #
-# #   # new_recipe = params.fetch("name")
-# #
-# #   @one_recipe = Recipe.create({:name => @recipe.name, :instruction_id => @instruction.id, :ingredient_id => @ingredient.id, :tag_id => @tag.id})
-# #
-# #   # @ingredients = Ingredient.all
-# #   # @instructions = Instruction.all
-# #   # @recipes = Recipe.all
-# #   # @tags = Tag.all
-# #   erb(:recipe_list)
-# # end
-# #
-# # # will show stuff in recipe_list.erb
-# # get('/add_recipe') do
-# #   # @ingredient = Ingredient.all
-# #   # @instruction = Instruction.all
-# #   @recipe = Recipe.all
-# #   # @tag = Tag.all
-# #   @one_recipe
-# #   erb(:recipe_list)
-# # end
-# #
-# # # display all recipes at that url using the list_recipes views
-# # post '/recipe_list' do
-# #   @recipe = Recipe.all
-# #   erb(:list_recipes)
-# # end
+get '/store/:id/view' do
+  @brands = Brand.all
+  @stores = Store.all
+  erb(:store)
+end
+
+post '/store/:id/view' do
+  @store = Store.find(params.fetch("id").to_i)
+  @brands = Brand.all
+  @stores = Store.all
+  erb(:store)
+end
+
+get('/store/:id/view')do
+  @store = Store.find(params.fetch("id").to_i())
+  @brands = Brand.all - @store.brands
+  erb(:store)
+end
+
+patch '/store/:id/view' do
+  @store = Store.find(params.fetch("id").to_i())
+  brand_ids = params['brand_ids'] || [] # 'or' statement, returns empty array if no brands checked
+  brand_ids.each do |brand_id|
+    new_brand = Brand.find(brand_id)
+    @store.brands.push(new_brand)
+  end
+  @brands = Brand.all - @store.brands
+  erb(:store)
+end
